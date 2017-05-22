d4723f653e400664b59ac971c581e784e7323a24
SimoTr84 <simone.trucco@gmail.com>
2017-05-22 16:22:42 +1000

    date and time set for today's events

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,12 +2,20 @@ class HomeController < ApplicationController
 
   def index
     @events = Event.all
-    
+
     @main_event = Event.all.first
 
     event_name = @main_event.name
     event_description = @main_event.description
 
+
+  end
+
+  def today
+    @today= Time.now.midnight
+    # @before_midnight = Time.now
+    @today_event = Event.where(created_at: @today)
+    # raise
   end
 
   def show

24f2aa392f468cdc9bd8ca79cf9955156814d881
Philg112 <phil.grujovski@gmail.com>
2017-05-22 17:59:32 +1000

    Booking button conditional done

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,8 +1,10 @@
 class BookingsController < ApplicationController
   def create
+    # TODO: Create booking functionality
+    raise "hell"
   end
   
   def delete
+    # TODO: Dlete booking functionality
   end
-  
 end

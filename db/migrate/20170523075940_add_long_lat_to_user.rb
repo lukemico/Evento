92d2c134bdf28324b8db4af1d07457e0cbdd96fb
Philg112 <phil.grujovski@gmail.com>
2017-05-24 19:13:22 +1000

    cya long lat

--- db/migrate/20170523075940_add_long_lat_to_user.rb
+++ db/migrate/20170523075940_add_long_lat_to_user.rb
@@ -1,7 +1,7 @@
 class AddLongLatToUser < ActiveRecord::Migration[5.0]
   def change
-    add_column :users, :longitude, :string
-    add_column :users, :latitude, :string
+    add_column :users, :longitude, :float
+    add_column :users, :latitude, :float
 
   end
 end
--- db/migrate/20170524063013_change_long_lat_to_float.rb
+++
@@ -1,8 +0,0 @@
-class ChangeLongLatToFloat < ActiveRecord::Migration[5.0]
-  def change
-    change_column :locations, :latitude, :float
-    change_column :locations, :longitude, :float
-    change_column :users, :longitude, :float
-    change_column :users, :latitude, :float
-  end
-end

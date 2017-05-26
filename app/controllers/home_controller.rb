f579da10de445bc3b9d72d2dc09f761b2269575e
Philg112 <phil.grujovski@gmail.com>
2017-05-26 10:55:25 +1000

    carousel working

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,12 @@ class HomeController < ApplicationController
   def index
     @events = Event.all
     @three_events = @events.sample(3)
-    @main_event = Event.all.sample()
+    main_event = Event.all.sample(10)
+    @images = []
+    main_event.each do |m|
+      @images.push([m.image, m.name, m.id])
+    end
+    
   end
 
   def show
